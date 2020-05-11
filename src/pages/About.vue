f93f15f45790b9eabb8efa072833d79aa64013dc
Montaserfzy <montaserfzy@gmail.com>
2020-05-12 00:28:33 +0300

    update the about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,20 +6,34 @@
             </h2>
             <div>
                 <blockquote>
-                    <Strong>Auto complete</Strong> widgets provides suggestions as following:
+                    <Strong>Auto complete</Strong> widget provides suggestions as following:
                 </blockquote>
                 <div class='list-group'>
                     <ul class="d-flex flex-column">
-                        <li class="list-group-item">While user type into the field. showing list for the suggestions result</li>
+                        <li class="list-group-item">While user type into the field. showing list for the suggestions
+                            result
+                        </li>
                         <li class="list-group-item">Have dynamic get url</li>
                         <li class="list-group-item">Have dynamic response delay timeout</li>
                         <li class="list-group-item">Have dynamic response data key match</li>
-                        <li class="list-group-item">User can select one item by clicking on it</li>
+                        <li class="list-group-item">User can select any item by clicking on it or moving to it by arrow
+                            up and down and click enter
+                        </li>
                         <li class="list-group-item">User can move on the items by arrow up and down</li>
-                        <li class="list-group-item">User can select item from the drop down list by clicking on it or click on enter</li>
                         <li class="list-group-item">User can select the input value by clicking enter</li>
-                        <li class="list-group-item">User can remove item by re clicking on selected item/tag or click on backspace</li>
+                        <li class="list-group-item">User can remove item by click on selected item/tag or click on
+                            backspace
+                        </li>
                         <li class="list-group-item">Close list by clicking out of the auto complete widget</li>
+                        <li class="list-group-item">On waiting time spinner loader coming up</li>
+                        <li class="list-group-item">More tags exceed the limit grouped one tag and show total items
+                            number
+                        </li>
+                        <li class="list-group-item">Item hovering background color is <code>#2398ff05</code></li>
+                        <li class="list-group-item">Arrow up and down change the background color to
+                            <code>#eeeeee</code></li>
+                        <li class="list-group-item">Selected item in drop down list have left border <code>2px solid
+                            #42b983</code></li>
                     </ul>
                 </div>
             </div>
@@ -47,9 +61,27 @@
             </blockquote>
 
             <pre>
-                <code class="hljs html"><span class="hljs-comment">&lt;!-- How to use --&gt;</span><br><br><span class="tag">import auto-complete from '../components/AutoComplete';</span><br><br><span
+                <code class="hljs html"><span class="hljs-comment">&lt;!-- How to use --&gt;</span><br><br><span
+                        class="tag">import auto-complete from '../components/AutoComplete';</span><br><br><span
                         class="tag">&lt;auto-complete <br>   uri='http://www.mocky.io/v2/5eb814fe2d00008600357be1'<br>   keyMatch='username'<br>   :maxInputTags=2<br>   :requestDelay=1000<br>/&gt;</span></code>
             </pre>
+        </section>
+        <hr>
+        <section class="mb-5">
+            <blockquote>
+                <Strong>Auto complete</Strong> TODO:
+            </blockquote>
+            <div class='list-group'>
+                <ul class="d-flex flex-column">
+                    <li class="list-group-item">Make the widget more dynamic by adding more props for coloring, spinner DOM, keys, ...etc.</li>
+                    <li class="list-group-item">Adding validation on input value.</li>
+                    <li class="list-group-item">Adding animation to drop down list and select un select item</li>
+                    <li class="list-group-item">Adding description on user input value if the value is already selected.</li>
+                    <li class="list-group-item">Showing button to save selection values and retrieve it any time.</li>
+                    <li class="list-group-item">Build it as a package to be able to used any where.</li>
+                </ul>
+            </div>
+
         </section>
     </article>
 </template>
